4198906be4d53017bedea1bc7304aaee96004aa5
Lawal-faruq <107109677+Lawal-faruq@users.noreply.github.com>
2025-12-19 09:46:23 +0100

    Create 02_Data_Validation&Cleaning.sql

---
+++ Scripts/02_Data_Validation&Cleaning.sql
@@ -0,0 +1,45 @@
+-- Data Validation & Cleaning
+
+-- Checking for Null values in all the columns
+-- Expected Result: Zeros in all the columns
+SELECT
+	SUM(CASE WHEN State IS NULL THEN 1 ELSE 0 END) AS null_state,
+	SUM(CASE WHEN City IS NULL THEN 1 ELSE 0 END) AS null_city,
+	SUM(CASE WHEN Order_Date IS NULL THEN 1 ELSE 0 END) AS null_order_date,
+	SUM(CASE WHEN Restaurant_Name IS NULL THEN 1 ELSE 0 END) AS null_restaurant,
+	SUM(CASE WHEN Location IS NULL THEN 1 ELSE 0 END) AS null_location,
+	SUM(CASE WHEN Category IS NULL THEN 1 ELSE 0 END) AS null_category,
+	SUM(CASE WHEN Dish_Name IS NULL THEN 1 ELSE 0 END) AS null_dish,
+	SUM(CASE WHEN Price_INR IS NULL THEN 1 ELSE 0 END) AS null_price,
+	SUM(CASE WHEN Rating IS NULL THEN 1 ELSE 0 END) AS null_rating,
+	SUM(CASE WHEN Rating_Count IS NULL THEN 1 ELSE 0 END) AS null_rating_count
+FROM swiggy_data;
+
+-- Blank or Empty Strings
+-- Expected Result:  Blank Query
+SELECT *
+FROM swiggy_data
+WHERE 
+	State = '' OR
+	City = '' OR
+	Restaurant_Name = '' OR
+	Location = '' OR
+	Category = '' OR
+	Dish_Name = ''
+
+
+-- Duplicate Detection
+SELECT
+	*, COUNT(*) AS CNT
+FROM swiggy_data
+GROUP BY  State, City, Order_Date, Restaurant_Name, Location, Category, Dish_Name, Price_INR, Rating, Rating_Count
+HAVING COUNT(*) > 1 
+
+-- Delete Duplicate record
+WITH CTE AS (
+SELECT *, ROW_NUMBER() OVER(PARTITION BY State, City, Order_Date, Restaurant_Name, Location, Category, 
+Dish_Name, Price_INR, Rating, Rating_Count ORDER BY (SELECT NULL)
+) AS rn
+FROM swiggy_data
+)
+DELETE FROM CTE WHERE rn>1;
